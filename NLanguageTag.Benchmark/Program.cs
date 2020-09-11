0598422fc09f9d1104d0e057e8200ac30eade10d
ExM <ExplorerMachine@gmail.com>
2020-09-12 00:11:36 +0300

    Benchmark of LanguageTag.Parse

---
+++ NLanguageTag.Benchmark/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace NLanguageTag.Benchmark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<Parse>();
+		}
+	}
+
+	[RPlotExporter]
+	[HtmlExporter]
+	[MinColumn, MaxColumn, MeanColumn, MedianColumn]
+	[InvocationCount(1000000, 1000)]
+	public class Parse
+	{
+		private int _languageIndex = 0;
+		private int _regionIndex = 0;
+		private int _scriptIndex = 0;
+		private string _text;
+
+		[GlobalSetup]
+		public void GlobalSetup()
+		{
+		}
+
+		[IterationSetup]
+		public void IterationSetup()
+		{
+			_text = string.Join("-",
+				SubtagSamples.Languages[_languageIndex],
+				SubtagSamples.Scripts[_scriptIndex],
+				SubtagSamples.Regions[_regionIndex]);
+
+			_languageIndex = (_languageIndex + 17) % SubtagSamples.Languages.Count;
+			_regionIndex = (_regionIndex + 19) % SubtagSamples.Regions.Count;
+			_scriptIndex = (_scriptIndex + 23) % SubtagSamples.Scripts.Count;
+		}
+
+		[Benchmark]
+		public LanguageTag LanguageScriptAndRegion() => LanguageTag.Parse(_text);
+	}
+}
---
+++ NLanguageTag.Benchmark/SubtagSamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace NLanguageTag.Benchmark
+{
+	public static class SubtagSamples
+	{
+		public static IReadOnlyList<string> Languages = readSubtags("Languages").ToList();
+		public static IReadOnlyList<string> Regions = readSubtags("Regions").ToList();
+		public static IReadOnlyList<string> Scripts = readSubtags("Scripts").ToList();
+		public static IReadOnlyList<string> Variants = readSubtags("Variants").ToList();
+		public static IReadOnlyList<string> ExtLanguages = readSubtags("ExtLanguages").ToList();
+
+		private static IEnumerable<string> readSubtags(string resourceName)
+		{
+			var fullResourceName = $"NLanguageTag.Benchmark.SubtagSamples.{resourceName}.txt";
+			using var stream = typeof(SubtagSamples).Assembly.GetManifestResourceStream(fullResourceName);
+			using var reader = new StreamReader(stream);
+
+			while(true)
+			{
+				var line = reader.ReadLine();
+				if(string.IsNullOrEmpty(line))
+					yield break;
+				else
+					yield return line;
+			}
+		}
+	}
+}
