4a05e276d1e0f4686b258341b91ba32a0285f265
Anatoliy Koperin <a.koperin@gmail.com>
2021-06-08 22:37:16 +0300

    fix tests

--- NLanguageTag.Tests/TestContent.cs
+++ NLanguageTag.Tests/TestContent.cs
@@ -10,22 +10,22 @@ namespace NLanguageTag.Tests
 		public static readonly Registry Registry = Registry.ReadCurrent();
 
 		public static IEnumerable<string> GetLanguages() =>
-			Registry.PrimaryLanguages.Select(_ => _.Subtag);
+			Registry.PrimaryLanguages.Select(_ => _.Subtag!);
 
 		public static IEnumerable<string> GetGrandfathered() =>
-			Registry.Grandfathered.Where(_ => _.PreferredValue != null).Select(_ => _.Tag);
+			Registry.Grandfathered.Where(_ => _.PreferredValue != null).Select(_ => _.Tag!);
 
 		public static IEnumerable<string> GetGrandfatheredNotSupported() =>
-			Registry.Grandfathered.Where(_ => _.PreferredValue == null).Select(_ => _.Tag);
+			Registry.Grandfathered.Where(_ => _.PreferredValue == null).Select(_ => _.Tag!);
 
 		public static IEnumerable<string> GetRegions() =>
-			Registry.Regions.Select(_ => _.Subtag);
+			Registry.Regions.Select(_ => _.Subtag!);
 
 		public static IEnumerable<string> GetScripts() =>
-			Registry.Scripts.Select(_ => _.Subtag);
+			Registry.Scripts.Select(_ => _.Subtag!);
 
 		public static IEnumerable<string> GetVariants() =>
-			Registry.Variants.Select(_ => _.Subtag);
+			Registry.Variants.Select(_ => _.Subtag!);
 
 		public static IEnumerable<string> GetExtLanguages() =>
 			Registry.ExtLanguages.Where(_ => _.PreferredValue != null).Select(_ => _.Prefix + "-" + _.Subtag);
